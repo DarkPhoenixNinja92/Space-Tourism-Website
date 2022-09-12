6af8a26095559459eb9b226522af4c417d77e83d
Shaun <scpour@gmail.com>
2022-09-12 18:32:04 -0400

    1.08

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap"
+import { Navbar, NavbarBrand, Nav } from "reactstrap";
 import Logo from '../images/shared/logo.svg';
 import './Header.css';
 
 const Header = () => {
     return(
-            <Navbar className="nav" dark color = 'primary' sticky = 'top'>
-                <NavbarBrand href="/">
-                <img className='icon' src= {Logo}/>
-                </NavbarBrand>
-                <Nav className="navbar">
-                    <Link className="navitm linkItm menu-itm home current" to='/'>Home</Link>
-                    <Link className="navitm linkItm menu-itm destination inactive" to='/destination'>Destination</Link>
-                    <Link className="navitm linkItm menu-itm crew inactive" to='/crew'>Crew</Link>
-                    <Link className="navitm linkItm menu-itm technology inactive" to='/technology'>Technology</Link>
-                </Nav>
-            </Navbar>
-    )
+        <Navbar className="nav" dark color = 'primary' sticky = 'top'>
+            <NavbarBrand href="/">
+            <img className='icon' src= {Logo}/>
+            </NavbarBrand>
+            <Nav className="navbar">
+                <Link className="navitm linkItm menu-itm home current" to='/'>Home</Link>
+                <Link className="navitm linkItm menu-itm destination inactive" to='/destination'>Destination</Link>
+                <Link className="navitm linkItm menu-itm crew inactive" to='/crew'>Crew</Link>
+                <Link className="navitm linkItm menu-itm technology inactive" to='/technology'>Technology</Link>
+            </Nav>
+        </Navbar>
+)
 }
 
 export default Header;
--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,7 +1,37 @@
+import './Destination.css';
+import Moon from '../images/destination/image-moon.png';
+import { Link } from 'react-router-dom';
+
 const Destination = () => {
     return (
-    <div>
-        <h1>Destination</h1>
+    <div className="data-set">
+        <div className="left">
+        <h1 className="title"><span>01</span> Pick Your Destination</h1>
+        <img src={Moon}/>
+        </div>
+        <div className='right'>
+            <ul>
+                <li>Moon</li>
+                <li>Mars</li>
+                <li>Europa</li>
+                <li>Titan</li>
+            </ul>
+            <h2 className='location'>Moon</h2>
+            <p className='descr'>
+            See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed.
+            While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+            </p>
+            <div className='info'>
+                <div className='distance'>
+                <h3 className='dist-title'>Avg. Distance</h3>
+                <p className='dist'>384,400 KM</p>
+                </div>
+                <div className='travel'>
+                    <h3 className='travel-title'>Est. Travel Time</h3>
+                    <p className='travel-para'>3 Days</p>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
