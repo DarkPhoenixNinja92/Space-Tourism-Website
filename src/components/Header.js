89c9f327717b0ce0bb808fd4bbdad5b00f3943eb
Shaun <scpour@gmail.com>
2022-09-11 20:45:48 -0400

    1.01

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
             <Navbar className="nav" dark color = 'primary' sticky = 'top'>
                 <Nav>
                     <NavItem className="navitm"><img className='float-start' src= {Logo}/></NavItem>
-                    <NavItem className="navitm">00 Home</NavItem>
-                    <NavItem className="navitm">01 Destination</NavItem>
-                    <NavItem className="navitm">02 Crew</NavItem>
-                    <NavItem className="navitm">03 Technology</NavItem>
+                    <NavItem className="navitm menu-itm">00 Home</NavItem>
+                    <NavItem className="navitm menu-itm">01 Destination</NavItem>
+                    <NavItem className="navitm menu-itm">02 Crew</NavItem>
+                    <NavItem className="navitm menu-itm">03 Technology</NavItem>
                 </Nav>
             </Navbar>
     )
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import DesktopHome from '../images/home/background-home-desktop.jpg';
+import './Home.css'
+
+const Home = () => {
+    
+}
+export default Home;
