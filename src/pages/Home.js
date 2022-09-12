d4f7fd0ad4c00f3387c663fc573321c0a591b4fb
Shaun <scpour@gmail.com>
2022-09-11 20:58:23 -0400

    1.02

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,19 @@ import DesktopHome from '../images/home/background-home-desktop.jpg';
 import './Home.css'
 
 const Home = () => {
-    
+    return(
+    <div className='cover'>
+        <h3 className='travel'>So, you want to travel to</h3>
+        <h1 className='title'>Space</h1>
+        <p className='para'>
+            Let’s face it; if you want to go to space, you might as well genuinely go to 
+  outer space and not hover kind of on the edge of it. Well sit back, and relax 
+  because we’ll give you a truly out of this world experience!
+  </p>
+  <div className='explore'>
+    <h2>Explore</h2>
+  </div>
+    </div>
+    );
 }
 export default Home;
