19f1c0e73853e046caf88207719827dbb2672640
Shaun <scpour@gmail.com>
2024-02-06 23:10:57 -0500

    first commit

---
+++ app/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href=""><span>00</span> Home</a></li>
+                <li><a href=""><span>01</span> Destination</a></li>
+                <li><a href=""><span>02</span> Crew</a></li>
+                <li><a href=""><span>03</span> Technology</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
