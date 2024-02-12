ba199fd98dfccd7f55e1ea9d229f676e18b591bc
Shaun <scpour@gmail.com>
2024-02-11 20:39:06 -0500

    Added data transition to crew page

--- app/crew/page.js
+++ app/crew/page.js
@@ -1,28 +1,54 @@
+"use client";
+import {useState} from 'react';
+import data from '../data.json';
 import Doug from '../img/image-douglas-hurley.png'
+import Mark from '../img/image-mark-shuttleworth.png'
+import Victor from '../img/image-victor-glover.png'
+import Anousheh from '../img/image-anousheh-ansari.png'
 import Image from 'next/image';
 
 const Crew = () => {
+    const [crewMember, setcrewMember] = useState(data.crew[0]);
+    const [crewMemberImg, setcrewMemberImg] = useState({Doug});
     return (
         <section className="crew">
             <h2 className="crew-title">
                 <span>02
                 </span>Meet your crew</h2>
-            <Image className='crew-member-img' src={Doug} width='177.12' height='222' alt='Douglas Hurley'/>
+            <Image className='crew-member-img' src={crewMemberImg} width='177.12' height='222' alt={crewMember.name}/>
             <div className="img-border">a</div>
             <div className="crew-btn-div">
-                <button className='crew-btn active-btn'></button>
-                <button className='crew-btn'></button>
-                <button className='crew-btn'></button>
-                <button className='crew-btn'></button>
+                <button className={`crew-btn ${crewMember == data.crew[0]
+                        ? 'active-btn'
+                        : ""}`} onClick={() => {
+                            setcrewMember(data.crew[0]);
+                            setcrewMemberImg(Doug)
+                        }}></button>
+                <button className={`crew-btn ${crewMember == data.crew[1]
+                        ? 'active-btn'
+                        : ""}`} onClick={() => {
+                            setcrewMember(data.crew[1]);
+                            setcrewMemberImg(Mark)
+                        }}></button>
+                <button className={`crew-btn ${crewMember == data.crew[2]
+                        ? 'active-btn'
+                        : ""}`} onClick={() => {
+                            setcrewMember(data.crew[2]);
+                            setcrewMemberImg(Victor)
+                        }}></button>
+                <button className={`crew-btn ${crewMember == data.crew[3]
+                        ? 'active-btn'
+                        : ""}`} onClick={() => {
+                            setcrewMember(data.crew[3]);
+                            setcrewMemberImg(Anousheh)
+                        }}></button>
             </div>
             <div className="person">
-                <h3 className='crew-member-title'>commander</h3>
-                <p className='crew-member-name'>Douglas Hurley</p>
+                <h3 className='crew-member-title'>{crewMember.role}</h3>
+                <p className='crew-member-name'>{crewMember.name}</p>
             </div>
             <p className="crew-txt">
-                Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and
-                former NASA astronaut. He launched into space for the third time as commander of
-                Crew Dragon Demo-2.
+                {crewMember.bio}
             </p>
         </section>
     );
