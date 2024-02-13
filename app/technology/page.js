a82e8354382e0a681cdfca632754930a408eae6a
Shaun <scpour@gmail.com>
2024-02-12 22:22:13 -0500

    Added data swap functionality to technology page

--- app/technology/page.js
+++ app/technology/page.js
@@ -1,7 +1,14 @@
+"use client";
+import {useState} from 'react';
+import data from '../data.json';
 import LaunchVehicle from '../img/image-launch-vehicle-landscape.jpg'
+import SpaceCapsule from '../img/image-space-capsule-landscape.jpg'
+import SpacePort from '../img/image-spaceport-portrait.jpg'
 import Image from "next/image";
 
 const Technology = () => {
+    const [technology, setTechnology] = useState(data.technology[0]);
+    const [technologyImg, setTechnologyImg] = useState({LaunchVehicle});
     return (
         <section className="technology">
             <h2 className="technology-title">
@@ -9,24 +16,36 @@ const Technology = () => {
                 </span>Space Launch 101</h2>
             <Image
                 className='launch-img'
-                src={LaunchVehicle}
+                src={technologyImg}
                 width='375'
                 height='170'
-                alt='launch vehicle'/>
+                alt={technology.name}/>
             <div className="launch-btn-div">
-                <button className='launch-btn active-launch-btn'>1</button>
-                <button className='launch-btn'>2</button>
-                <button className='launch-btn'>3</button>
+                <button className={`launch-btn ${technology == data.technology[0]
+                        ? 'active-launch-btn'
+                        : ""}`} onClick={() => {
+                            setTechnology(data.technology[0]);
+                            setTechnologyImg(LaunchVehicle)
+                        }}>1</button>
+                <button className={`launch-btn ${technology == data.technology[1]
+                        ? 'active-launch-btn'
+                        : ""}`} onClick={() => {
+                            setTechnology(data.technology[1]);
+                            setTechnologyImg(SpaceCapsule)
+                        }}>2</button>
+                <button className={`launch-btn ${technology == data.technology[2]
+                        ? 'active-launch-btn'
+                        : ""}`} onClick={() => {
+                            setTechnology(data.technology[2]);
+                            setTechnologyImg(SpacePort)
+                        }}>3</button>
             </div>
             <div className="terminology-div">
                 <h3 className="terminology-title">THE TERMINOLOGY…</h3>
-                <p className="terminology-name">launch vehicle</p>
+                <p className="terminology-name">{technology.name}</p>
             </div>
             <p className="technology-txt">
-                A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a
-                payload from Earth's surface to space, usually to Earth orbit or beyond. Our
-                WEB-X carrier rocket is the most powerful in operation. Standing 150 metres
-                tall, it's quite an awe-inspiring sight on the launch
+                {technology.description}
             </p>
         </section>
     );
